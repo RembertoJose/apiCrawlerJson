d1df9aab6ba6ae91e6cadd4f1aebf23fcfe05850
RembertoNunez <remnunez@csumb.edu>
2019-10-16 09:24:08 -0400

    Initial Commit

---
+++ tableViewJSONHwRemberto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewJSONHwRemberto
+//
+//  Created by Remberto  Nunez  on 10/16/19.
+//  Copyright © 2019 Remberto  Nunez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
